016fa613297833714ba8308dbd4dd6e4025a0ffb
NabiyaAman <nabiyaaman@gmail.com>
2026-06-05 10:47:44 +0530

    Create src package

---
+++ src/__init__.py
@@ -0,0 +1,6 @@
+from src.eml_parser import EMLParser
+from src.excel_reader import ExcelReader
+from src.comparator import EmailComparator, ComparisonResult
+from src.test_reporter import TestReporter
+
+__all__ = ['EMLParser', 'ExcelReader', 'EmailComparator', 'ComparisonResult', 'TestReporter']
